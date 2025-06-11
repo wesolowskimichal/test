20015b38cb9e339e012a6453f99e18fd73b54b3e
Michal Wesolowski <73594009+wesolowskimichal@users.noreply.github.com>
2025-06-11 12:35:23 +0200

    Update hello_world2.sql

--- models/hello_world2.sql
+++ models/hello_world2.sql
@@ -2,5 +2,5 @@
 
 SELECT
     'Hello world2'                           AS greeting
-FROM
-    schema_that_does_not_exist.nonexistent_table
+-- FROM
+--     schema_that_does_not_exist.nonexistent_table
